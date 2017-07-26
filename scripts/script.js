accde82caaab66e0ba82091d3e4302415609fbfd
Alex Jung <alexandermjung89@gmail.com>
2017-07-26 10:13:20 -0500

    added objects example by ref and val

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+console.log('js still sourced');
+//[ name, favoritAnimal]
+var people = [
+    {
+        name: 'Canin', 
+        favoriteAnimal: 'Uniflitten'
+    },
+        {
+        name: 'Charly', 
+        favoriteAnimal: 'Angler Fish'
+    },
+        {
+        name: 'Bobby', 
+        favoriteAnimal: 'Liger'
+    },
+        {
+        name: 'Luke', 
+        favoriteAnimal: 'Orca'
+    },
+];
+
+// console.log(people);
+
+// for (var i = 0; i < people.length; i++) {
+//     console.log(people[i]);
+//    var childPersonArray = people[i];
+//     console.log(childPersonArray[0] + '\'s favorite animal is the '+ childPersonArray[1]);
+
+   
+// }
